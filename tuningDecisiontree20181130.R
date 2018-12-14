60d4ad21055cd2ff7272ffaee871d9ad75779837
Simon Danielsson <simon.danielsson@soprasteria.com>
2018-12-14 09:30:15 +0100

    Added visuals

--- tuningDecisiontree20181130.R
+++ tuningDecisiontree20181130.R
@@ -19,32 +19,55 @@ library(ggplot2)
 library(lubridate)
 library(scales)
 
+######################################################### Import Data ####################################################
 
 #load data
-calldata <- read.csv("df1177_5dec.csv") %>%
-  select(-"X",-"lagwDay",-"lagMonth",-"lagMonth_NA",-"lagwDay_NA",-"logLeadSamtal")
+calldata <- read.csv("df1177_13dec.csv",stringsAsFactors = F) %>%
+  select(-"X",-"predictDay")
+  #select(-"X",-"lagwDay",-"lagMonth",-"lagMonth_NA",-"lagwDay_NA")
 
 calldata$Date <- as.Date(calldata$Date)
 calldata <- calldata %>%
-  mutate(yeardayPredict = yday(Date + predictHorizon), weekPredict = week(Date + predictHorizon))
+  mutate(yeardayPredict = yday(Date + predictHorizon))
+  #mutate(yeardayPredict = yday(Date + predictHorizon), weekPredict = week(Date + predictHorizon))
 
-calldata$Date <- as.Date(calldata$Date)
+calldata2 <- calldata[,-grep("_twoWeek",colnames(calldata))]
+calldata2 <- calldata2[,-grep("_threeWeek",colnames(calldata2))]
+#calldata2 <- calldata2[,-grep("lagwDay_",colnames(calldata2))]
+#calldata2 <- calldata2[,-grep("predictwDay_",colnames(calldata2))]
+calldata <- calldata2
 
-#calldata2 <- calldata[,-grep("year",colnames(calldata))]
-#calldata2 <- calldata2[,-grep("_month",colnames(calldata))]
-#calldata <- calldata2
+######################################################### Make Final Pred Data ####################################################
+
+fromDate <- '2017-05-01'
+toDate <- '2017-09-01'
+  
+#Pick out one month for final prediction
+calldataFinalPredAll <- calldata%>%
+  filter((Date>= fromDate& Date<toDate))
+
+#Remove the month used for final prediction
+calldata <- calldata%>%
+  filter(!(Date>= fromDate& Date<toDate))  
+
+######################################################### Simple Training Loop ####################################################
+
 
 resultSimpMod <- data.frame()
 resultImportance <- data.frame()
+resultPredict <- data.frame()
 
 
 for (horizon in sort(unique(calldata$predictHorizon))) {
   
   currentSet <- calldata[calldata$predictHorizon == horizon, ]
+  
+  calldataFinalPred <- calldataFinalPredAll[calldataFinalPredAll$predictHorizon == horizon,]
   #testFinal <- testData[testData$predictHorizon == horizon, ]
   
-  set.seed(1177)
+  set.seed(1)
   sampleSets <- sample(c(1:3),size = nrow(currentSet),replace = T, prob = c(0.7,0.2,0.1))
+  #sampleSets <- sample(c(1:2),size = nrow(currentSet),replace = T, prob = c(0.7,0.3))
   #train_default <- sample_frac(currentSet,0.7,replace=F)
   #test_default <-  currentSet[-as.numeric(rownames(train_default)),]
   train_default <-  currentSet[sampleSets == 1, ]
@@ -103,6 +126,24 @@ for (horizon in sort(unique(calldata$predictHorizon))) {
   
   #preparing matrix
   dtestFinal <- xgb.DMatrix(data = new_test,label=test_label)
+ 
+   ######################################################### Final Pred Data ####################################################
+  
+  calldataFinalPred <- calldataFinalPred[complete.cases(calldataFinalPred), ]
+  
+  precentPred <- calldataFinalPred %>%
+    mutate(logLeadSamtal = log(leadSamtal)) %>%
+    select(-"Date",-"samtalLog",-"samtal",-"leadSamtal",-"predictHorizon")
+  
+  #convert data frame to data table
+  setDT(precentPred)
+  
+  #using one hot encoding
+  labels_pred <- precentPred$logLeadSamtal
+  new_tr_pred <- as.matrix(precentPred[,-c("logLeadSamtal")])
+  
+  #preparing matrix
+  dtestPred <- xgb.DMatrix(data = new_tr_pred,label = labels_pred)
   
   
   ##################################################### Simple training with default parameters #####################################################
@@ -148,6 +189,8 @@ for (horizon in sort(unique(calldata$predictHorizon))) {
   
   #model prediction
   xgbpred <- predict(xgb1,dtestFinal)
+  predFinal <- predict(xgb1,dtestPred)
+  #xgbpred <- predict(xgb1,dtest)
   
   #expLabel <- exp(as.numeric(ts_label))
   #expPred <- exp(as.numeric(xgbpred))
@@ -157,7 +200,9 @@ for (horizon in sort(unique(calldata$predictHorizon))) {
   #err <- rmse(exp(as.numeric(ts_label)), exp(as.numeric(xgbpred)))
   
   testFinal$PredictRes <- xgbpred
-   
+  calldataFinalPred$PredictRes <- predFinal 
+  #test_default$PredictRes <- xgbpred
+  resultPredict <- rbind(resultPredict,calldataFinalPred)
   resultSimpMod <- rbind(resultSimpMod,testFinal)
   
   mat <- xgb.importance(feature_names = colnames(new_tr),model = xgb1)
@@ -166,22 +211,45 @@ for (horizon in sort(unique(calldata$predictHorizon))) {
   
 }
 
-
-
+##################################################### Error Statistics #####################################################
+#Error simple model for each predict horizon
 errorSimpMod <- resultSimpMod%>%
   group_by(predictHorizon)%>%
   summarize(error = rmse(leadSamtal, exp(as.numeric(PredictRes))),
-            in_100 =  sum((abs(leadSamtal-exp(as.numeric(PredictRes))))<100)/n(),
+            in_200 = sum((abs(leadSamtal-exp(as.numeric(PredictRes))))<200)/n(), 
+            in_100 =  sum((abs(leadSamtal-exp(as.numeric(PredictRes))))<100)/n(), 
+            in_50 =  sum((abs(leadSamtal-exp(as.numeric(PredictRes))))<50)/n(),
             rmspe = rmse(leadSamtal, exp(as.numeric(PredictRes)))/mean(leadSamtal))
 
+#Error simple model for each yearday
+errorSimpModTime <- resultSimpMod%>%
+  group_by(yeardayPredict)%>%
+  summarize(error = rmse(leadSamtal, exp(as.numeric(PredictRes))),
+            in_200 = sum((abs(leadSamtal-exp(as.numeric(PredictRes))))<200)/n(), 
+            in_100 =  sum((abs(leadSamtal-exp(as.numeric(PredictRes))))<100)/n(), 
+            in_50 =  sum((abs(leadSamtal-exp(as.numeric(PredictRes))))<50)/n(),
+            rmspe = rmse(leadSamtal, exp(as.numeric(PredictRes)))/mean(leadSamtal),
+            nObs = n())
 
+#Error simple model for removed predicted period
+errorPred <- resultPredict%>%
+  group_by(predictHorizon)%>%
+  summarize(error = rmse(leadSamtal, exp(as.numeric(PredictRes))),
+            in_200 = sum((abs(leadSamtal-exp(as.numeric(PredictRes))))<200)/n(), 
+            in_100 =  sum((abs(leadSamtal-exp(as.numeric(PredictRes))))<100)/n(), 
+            in_50 =  sum((abs(leadSamtal-exp(as.numeric(PredictRes))))<50)/n(),
+            rmspe = rmse(leadSamtal, exp(as.numeric(PredictRes)))/mean(leadSamtal))
 
-meanDay <- resultSimpMod%>%
-  group_by(Date)%>%
-  summarize(meanPredict = mean(exp(PredictRes)), truth = mean(leadSamtal))%>%
-  ggplot(aes(Date))+geom_line(aes(y=meanPredict,colour="predict"))+geom_line(aes(y=truth,colour="trueValue"))
+##################################################### Result Visualisations #####################################################
 
-plot <- ggplot(subset(resultSimpMod, predictHorizon %in% c(4,5,6,7)), aes(x = Date+predictHorizon))+
+#Plot time axis of whole data set 
+plot <- ggplot(subset(calldata, predictHorizon %in% c(1)), aes(x = Date+predictHorizon))+
+  #geom_line(aes(y = exp(PredictRes),colour="prediction")) + 
+  geom_line(aes(y = leadSamtal,colour="trueValue"))+
+  xlab("Predicted day")+ylab("Number of Calls")
+
+#Plot prediction for sepcific horizon
+plot <- ggplot(subset(resultPredict, predictHorizon %in% c(2,8,9,10)), aes(x = Date+predictHorizon))+
    geom_line(aes(y = exp(PredictRes),colour="prediction")) + 
   geom_line(aes(y = leadSamtal,colour="trueValue"))+
   xlab("Predicted day")+ylab("Number of Calls")#+
@@ -189,24 +257,55 @@ plot <- ggplot(subset(resultSimpMod, predictHorizon %in% c(4,5,6,7)), aes(x = Da
   
 plot + facet_grid(predictHorizon~.)
 
-write.csv(resultSimpMod, file = "resultSimpleMod20181207.csv")
+#Plot prediction for sepcific horizon
+meanPred <- mean(exp(resultPredict$PredictRes))
+meanTrue <- mean(resultPredict$leadSamtal)
+sdPred <- sd(exp(resultPredict$PredictRes))
+sdTrue <- sd(resultPredict$leadSamtal)
+  
+resultPredict%>%
+  mutate(predicSamtal = exp(PredictRes))%>%
+  select(leadSamtal,predicSamtal)%>%
+  gather(key = "type",value = "samtal")%>%
+  ggplot(aes(samtal,fill=type))+
+  geom_histogram(alpha = 0.5, aes(y = ..density..),position = "identity")+
+  stat_function(fun = dnorm, args = list(mean =meanPred, sd = sdPred)) +
+  stat_function(fun = dnorm, args = list(mean =meanTrue, sd = sdTrue))
+
+
+write.csv(resultPredict, file = "resultSimpMod181214_data13dec.csv")
+
+
 
 #view variable importance plot for specific horizon
 resultImportance %>%
   filter(horizon == 7)%>%
   setDT()%>%
-  xgb.plot.importance() 
+  xgb.plot.importance()
+
+
+
+resultImportanceold%>%
+  group_by(horizon)%>%
+  arrange(desc(Gain))%>%
+  dplyr::top_n(10,wt=Gain)%>%
+  filter(horizon %in% c(1,7,14))%>%
+  ggplot(aes(y=Gain, x=reorder(Feature,-Gain), color=factor(horizon), fill=factor(horizon))) +
+  geom_bar(stat="identity") +   
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))+
+  facet_wrap(~horizon)
 
 resultTuneMod <- data.frame()
+resultTunePred <- data.frame()
 bestParam <- data.frame()
 
 
 for (horizon in sort(unique(calldata$predictHorizon))) {
  
   currentSet <- calldata[calldata$predictHorizon == horizon, ]
-
+  calldataFinalPredTune <- calldataFinalPredAll[calldataFinalPredAll$predictHorizon == horizon,]
   
-  set.seed(1177)
+  set.seed(1)
   sampleSets <- sample(c(1:2),size = nrow(currentSet),replace = T, prob = c(0.7,0.3))
   train_default <-  currentSet[sampleSets == 1, ]
   test_default <-  currentSet[sampleSets == 2, ]
@@ -226,11 +325,21 @@ for (horizon in sort(unique(calldata$predictHorizon))) {
     mutate(logLeadSamtal = log(leadSamtal)) %>%
     select(-"Date",-"samtalLog",-"samtal",-"leadSamtal",-"predictHorizon")
   
+  ######################################################### Final Pred Data ####################################################
+  
+  calldataFinalPredTune <- calldataFinalPredTune[complete.cases(calldataFinalPredTune), ]
+  
+  precentPred <- calldataFinalPredTune %>%
+    mutate(logLeadSamtal = log(leadSamtal)) %>%
+    select(-"Date",-"samtalLog",-"samtal",-"leadSamtal",-"predictHorizon")
+  
+  
   ######################################################### Tuning Data ####################################################
   
   #create tasks
   traintask <- makeRegrTask(data =train,target = "logLeadSamtal")
   testtask <- makeRegrTask(data = test,target = "logLeadSamtal")
+  predTast <- makeRegrTask(data = precentPred,target = "logLeadSamtal")
   
   #create learner
   lrn <- makeLearner("regr.xgboost",predict.type = "response")
@@ -255,7 +364,7 @@ for (horizon in sort(unique(calldata$predictHorizon))) {
   rdesc <- makeResampleDesc("CV",stratify = F ,iters=10L)
   #GrowingWindowCV som resample metod tar as lång tid. 
   #search strategy
-  ctrl <- makeTuneControlRandom(maxit = 50L)
+  ctrl <- makeTuneControlRandom(maxit = 10L)
   
   #parallelLibrary('mlr')
   #parallelStartSocket(cpus = 3)
@@ -279,8 +388,10 @@ for (horizon in sort(unique(calldata$predictHorizon))) {
   
   #predict model
   xgpred <- predict(xgmodel,testtask)
-  test_default$PredictRes <- xgpred$data$response 
+  predFinal <- predict(xgmodel,predTast)
   
+  test_default$PredictRes <- xgpred$data$response 
+  calldataFinalPredTune$PredictRes <- predFinal$data$response
   #error <- rmse(exp(xgpred$data$truth),exp(xgpred$data$response))
   #380 for count:poisson regression
   #375.8704 for reg:linear regression log samtal. 378.3714 when samtal
@@ -293,6 +404,7 @@ for (horizon in sort(unique(calldata$predictHorizon))) {
   
   
   resultTuneMod <- rbind(resultTuneMod,test_default)
+  resultTunePred <- rbind(resultTunePred,calldataFinalPredTune)
   
 }
   
@@ -301,7 +413,35 @@ errorTuneMod <- resultTuneMod %>%
   summarize(error = rmse(leadSamtal, exp(as.numeric(PredictRes))),
             in_100 =  sum((abs(leadSamtal-exp(as.numeric(PredictRes))))<100)/n())
 
+errorTuneModPred <- resultTunePred %>%
+  group_by(predictHorizon)%>%
+  summarize(error = rmse(leadSamtal, exp(as.numeric(PredictRes))),
+            in_100 =  sum((abs(leadSamtal-exp(as.numeric(PredictRes))))<100)/n())
 
-resultDf %>%
-  filter(predictHorizon >6 & predictHorizon < 15) %>%
-  summarize(rmse(actual,predicted))
+ 
+
+
+#Plot prediction for sepcific horizon
+plot <- ggplot(subset(resultTunePred, predictHorizon %in% c(4,7,9,13)), aes(x = Date+predictHorizon))+
+  geom_line(aes(y = exp(PredictRes),colour="prediction")) + 
+  geom_line(aes(y = leadSamtal,colour="trueValue"))+
+  xlab("Predicted day")+ylab("Number of Calls")#+
+#scale_x_date(date_breaks = "1 month", labels=date_format(format = "%Y-%m-%d"),limits = as.Date(c('2017-01-01','2018-01-01')))
+
+plot + facet_grid(predictHorizon~.)
+
+
+summaryDate <- resultTunePred%>%
+  group_by(Date)%>%
+  summarise(meanPred = mean(exp(PredictRes)),trueVal = mean(samtal),minPred = min(exp(PredictRes)),maxPred = max(exp(PredictRes)),sdPred = sd(exp(PredictRes)))
+
+resultTunePred%>%
+  group_by(Date)%>%
+  summarise(meanPred = mean(exp(PredictRes)),trueVal = mean(samtal))%>%
+  ggplot(aes(x = Date))+
+  geom_line(aes(y = meanPred,colour="prediction")) + 
+  geom_line(aes(y = trueVal,colour="trueValue"))+
+  xlab("Predicted day")+ylab("Number of Calls")#+
+
+
+
